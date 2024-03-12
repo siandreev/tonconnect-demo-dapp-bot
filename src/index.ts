a35f8c2224a6ad730e0554d352364eb5366afdb4
siandreev <andreev.sergey.i@yandex.ru>
2024-03-12 17:07:24 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import {bot} from "./bot";
+
+bot.onText(/\/connect/, msg => {
+
+});
+
+bot.onText(/\/send_tx/, msg => {
+
+});
+
+bot.onText(/\/disconnect/, msg => {
+
+});
+
+bot.onText(/\/my_wallet/, msg => {
+
+});
